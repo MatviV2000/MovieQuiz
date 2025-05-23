2586467748bea4cecfc9d257124d470c804ef165
VasiliyMatvienko <matvienko.vasiliy.a@gmail.com>
2025-05-23 13:33:56 +0300

    Поправил отступы

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -10,15 +10,15 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private struct QuizStepViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
+        let image: UIImage
+        let question: String
+        let questionNumber: String
     }
     
     private struct QuizResultsViewModel {
-      let title: String
-      let text: String
-      let buttonText: String
+        let title: String
+        let text: String
+        let buttonText: String
     }
     
     @IBOutlet weak private var noButton: UIButton!
@@ -70,8 +70,8 @@ final class MovieQuizViewController: UIViewController {
         QuizQuestion(image: "Vivarium",
                      text: "Рейтинг этого фильма больше чем 6?",
                      correctAnswer: false),
-        ]
-
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -101,19 +101,19 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showNextQuestionOrResults() {
-      if currentQuestionIndex == questions.count - 1 {
-          let viewModel = QuizResultsViewModel(title: "Этот раунд окончен!",
-                                               text: "Ваш результат \(correctAnswers)/10",
-                                               buttonText: "Сыграть еще раз")
-          show(quiz: viewModel)
-      } else {
-          currentQuestionIndex += 1
-          
-          let nextQuestion = questions[currentQuestionIndex]
-          let viewModel = convert(model: nextQuestion)
-          
-          show(quiz: viewModel)
-      }
+        if currentQuestionIndex == questions.count - 1 {
+            let viewModel = QuizResultsViewModel(title: "Этот раунд окончен!",
+                                                 text: "Ваш результат \(correctAnswers)/10",
+                                                 buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
     }
     
     private func showAnswerResult(isCorrect: Bool) {
@@ -125,7 +125,7 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         imageView.layer.cornerRadius = 20
-    
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
         }
@@ -136,7 +136,7 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-
+        
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
@@ -145,12 +145,12 @@ final class MovieQuizViewController: UIViewController {
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
         }
-
+        
         alert.addAction(action)
-
+        
         self.present(alert, animated: true, completion: nil)
     }
-
+    
 }
 
 /*
@@ -215,4 +215,4 @@ final class MovieQuizViewController: UIViewController {
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-*/
+ */
